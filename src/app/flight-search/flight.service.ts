8ea464f18c035ef0fd51985d2527337934e5a1de
mikezks <23002661+mikezks@users.noreply.github.com>
2023-10-17 14:32:56 +0200

    feat: flight service http data access

---
+++ src/app/flight-search/flight.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Flight } from '../model/flight';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FlightService {
+  private http = inject(HttpClient);
+
+  find(from: string, to: string): Observable<Flight[]> {
+    const url = 'https://demo.angulararchitects.io/api/flight';
+
+    const headers = {
+      Accept: 'application/json',
+    };
+
+    const params = { from, to };
+
+    return this.http.get<Flight[]>(url, { headers, params })
+  }
+}
