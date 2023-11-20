69cafbce0bfa93acc9264e33c149abd1d8a57c6b
mikezks <23002661+mikezks@users.noreply.github.com>
2023-11-20 10:56:17 +0100

    feat: routing essentials, routes, links, placeholder

---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./core/features/home/home.component";
+import { FlightSearchComponent } from "./flight-booking/features/flight-search/flight-search.component";
+
+export const APP_ROUTES: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'flight-search',
+    component: FlightSearchComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+]
