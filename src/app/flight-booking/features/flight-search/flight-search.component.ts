497fa5660f2cf032cd7e1623ccc053a5c5f3d55b
mikezks <23002661+mikezks@users.noreply.github.com>
2023-11-20 14:16:25 +0100

    fix: flight search method

--- src/app/flight-booking/features/flight-search/flight-search.component.ts
+++ src/app/flight-booking/features/flight-search/flight-search.component.ts
@@ -30,17 +30,6 @@ export class FlightSearchComponent {
   private flightService = inject(FlightService);
 
   search(): void {
-    const url = 'https://demo.angulararchitects.io/api/flight';
-
-    const headers = {
-      Accept: 'application/json',
-    };
-
-    const params = {
-      from: this.from,
-      to: this.to,
-    };
-
     this.flightService.find(this.from, this.to).subscribe({
       next: (flights) => {
         this.flights = flights;
